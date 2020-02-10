d0600c67ad30e2fb655f270b861c87ef219752c6
Borja Cabeza <borjacabezarozas@gmail.com>
2020-02-10 13:17:43 +0100

    Commit - Ejercicios Calculadora y Letra DNI

---
+++ Lab.WebApplication.Calculadora/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada a un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("Lab.WebApplication.Calculadora")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Lab.WebApplication.Calculadora")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("24833dca-9b2a-4efa-8d62-d3e4c2adcb3c")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Puede especificar todos los valores o aceptar los valores predeterminados de los números de compilación y de revisión 
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Lab.WebApplication.LetraDNI/Scripts/app.js
@@ -0,0 +1,16 @@
+﻿var app = {
+    Letras: ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'],
+    Init: function () {
+        document.getElementById('dni').value = '';
+        document.getElementById('b1').onclick = function () {
+            app.Process.CalcularDNI();
+        };
+    },
+    Process: {
+        CalcularDNI: function () {
+            var numero = parseFloat(document.getElementById('dni').value);
+            var posicion = numero % 23;
+            document.getElementById('res').innerHTML = numero + app.Letras[posicion];
+        }
+    }
+};
